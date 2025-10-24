768a677e74b19db1d4c96a8a65500e203ee5647f
tdat1465 <dat1405012005@gmail.com>
2025-10-24 20:04:37 +0700

    template engine

---
+++ src/main.js
@@ -0,0 +1,34 @@
+import express from 'express'
+import path from 'path'
+import morgan from 'morgan'
+import { engine as handlebars } from 'express-handlebars'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const app = express()
+const port = 3000
+
+// Static files
+app.use(express.static(path.join(__dirname, 'public')))
+
+// HTTP logger
+app.use(morgan('combined'))
+
+// Template engine
+app.engine('hbs', handlebars({ extname: '.hbs' }))
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'resources', 'views'))
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.get('/news', (req, res) => {
+  res.render('news')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+// nếu k có nodemon thì khi sửa file main.js thì phải dừng server và chạy lại lệnh node main.js để server nhận thay đổi
